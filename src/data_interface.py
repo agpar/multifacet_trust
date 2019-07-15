493080ae7bdc69e08b716a1af1df1bc2167c94e0
aparmentier <a.g.parmentier@gmail.com>
2019-07-15 10:24:40 -0400

    New: Added nicer data interface.
    The YelpData class stores the json data read in and can reproduce it
    in a nice format (it creates a nested structure user->reiews->business
    when data is retrieved). It also can produce a list of RatingTuples
    which are ready to be fed into a RS.

--- src/data_interface.py
+++ src/data_interface.py
@@ -13,7 +13,10 @@ a subset before doing all the dev work.
 """
 from os import path
 from collections import defaultdict
+from collections import namedtuple
 import json
+import copy
+
 
 NUM_USERS = 10000
 DATA_DIR = '/home/alex/Documents/datasets/yelp'
@@ -30,55 +33,99 @@ else:
     REVIEW_FILE = path.join(DATA_DIR, 'review_sample.json')
     TIP_FILE = path.join(DATA_DIR, 'tip_sample.json')
 
+RatingTuple = namedtuple('Rating', 'user_id item_id score')
 
-# Indexed by user_id.
-users = {}
-with open(USERS_FILE, 'r') as f:
-    num_read = 0
-    for line in f:
-        user = json.loads(line)
-        users[user['user_id']] = user
-        num_read += 1
-        if num_read == NUM_USERS:
-            break
-user_ids = set(users.keys())
+class YelpData:
+    def __init__(self, users, reviews, businesses):
+        self._users = users
+        self._reviews = reviews
+        self._businesses = businesses
+        self._rating_tuples = []
+
+    def get_user(self, user_id):
+        user = self._users[user_id]
+        if user.get('reviews'):
+            return user
+        reviews = self._reviews[user_id]
+        user['reviews'] = []
+        for review in reviews:
+            business = self._businesses.get(review['review_id'])
+            review['business'] = business
+            user['reviews'].append(review)
+        return user
+
+    def users(self):
+        for user in self._users.values():
+            yield self.get_user(user['user_id'])
+
+    def rating_tuples(self):
+        if self._rating_tuples:
+            return self.self._rating_tuples
+
+        rating_tuples = []
+        for user in self._users.values():
+            for review in self._reviews[user['user_id']]:
+                user_id = user['user_id']
+                item_id = review['business_id']
+                score = review['stars']
+                rating_tuples.append(RatingTuple(user_id, item_id, score))
+        self._rating_tuples = rating_tuples
+        return rating_tuples
 
 
-# Indexed by user_id
-reviews = defaultdict(list)
-with open(REVIEW_FILE, 'r') as f:
-    for line in f:
-        review = json.loads(line)
-        if review['user_id'] in user_ids:
-            review['text'] = ''
-            reviews[review['user_id']].append(review)
-reviewed_business_ids = dict({r['business_id']: r['review_id'] for rlist in reviews.values() for r in rlist})
+def read_data(read_sample=READ_SAMPLE):
+    # Indexed by user_id.
+    users = {}
+    with open(USERS_FILE, 'r') as f:
+        num_read = 0
+        for line in f:
+            user = json.loads(line)
+            users[user['user_id']] = user
+            num_read += 1
+            if num_read == NUM_USERS:
+                break
+    user_ids = set(users.keys())
+
+    # Indexed by user_id
+    reviews = defaultdict(list)
+    with open(REVIEW_FILE, 'r') as f:
+        for line in f:
+            review = json.loads(line)
+            if review['user_id'] in user_ids:
+                review['text'] = ''
+                reviews[review['user_id']].append(review)
+    reviewed_business_ids = dict({r['business_id']: r['review_id'] for rlist in reviews.values() for r in rlist})
+
+    # Indexed by review_id
+    businesses = {}
+    with open(BUSINESS_FILE) as f:
+        for line in f:
+            business = json.loads(line)
+            if business['business_id'] in reviewed_business_ids.keys():
+                businesses[reviewed_business_ids[business['business_id']]] = business
+
+    return YelpData(users, reviews, businesses)
 
 
-# Indexed by review_id
-businesses = {}
-with open(BUSINESS_FILE) as f:
-    for line in f:
-        business = json.loads(line)
-        if business['business_id'] in reviewed_business_ids.keys():
-            businesses[reviewed_business_ids[business['business_id']]] = business
-
-
-def save_sample():
+def save_sample(users, reviews, businesses):
     """Write out the NUM_USERS samples so they can be used again later."""
     USER_SAMPLE_PATH = path.join(DATA_DIR, 'user_sample.json')
-    BUSINESS_SAMPLE_PATH = path.join(DATA_DIR, 'business_sample.json')
     REVIEW_SAMPLE_PATH = path.join(DATA_DIR, 'review_sample.json')
+    BUSINESS_SAMPLE_PATH = path.join(DATA_DIR, 'business_sample.json')
 
     with open(USER_SAMPLE_PATH, 'w') as f:
         for user in users.values():
+            if user.get('reviews'):
+                del user['reviews']
             f.write(json.dumps(user)+"\n")
-
-    with open(BUSINESS_SAMPLE_PATH, 'w') as f:
-        for business in businesses.values():
-            f.write(json.dumps(business)+"\n")
 
     with open(REVIEW_SAMPLE_PATH, 'w') as f:
         for user_reviews in reviews.values():
             for review in user_reviews:
+                if review.get('business'):
+                    del review['business']
                 f.write(json.dumps(review)+"\n")
+
+    with open(BUSINESS_SAMPLE_PATH, 'w') as f:
+        for business in businesses.values():
+            f.write(json.dumps(business)+"\n")
