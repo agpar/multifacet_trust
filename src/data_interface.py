0c6d90ac077c90ca47ac16ab8b5938fedc41a01f
aparmentier <a.g.parmentier@gmail.com>
2019-07-17 10:38:21 -0400

    WIP: More trust indicators.

--- src/data_interface.py
+++ src/data_interface.py
@@ -30,7 +30,17 @@ class YelpData:
         self._reviews = reviews
         self._tips = tips
         self._businesses = businesses
+
         self._rating_tuples = []
+        self._reviews_by_item = defaultdict(list)
+        for review_list in self._reviews.values():
+            for review in review_list:
+                self._reviews_by_item[review['business_id']].append(review)
+
+        self._tips_by_item = defaultdict(list)
+        for tip_list in self._tips.values():
+            for tip in tip_list:
+                self._tips_by_item[tip['business_id']].append(tip)
 
     def get_user(self, user_id):
         user = self._users[user_id]
@@ -49,6 +59,26 @@ class YelpData:
             user['tips'].append(tip)
 
         return user
+
+    def get_reviews_for_item(self, business):
+        if isinstance(business, dict):
+            key = business['business_id']
+        elif isinstance(business, str):
+            key = business
+        else:
+            raise Exception("'business' is not a string or a dict")
+
+        return self._reviews_by_item[key]
+
+    def get_tips_for_item(self, business):
+        if isinstance(business, dict):
+            key = business['business_id']
+        elif isinstance(business, str):
+            key = business
+        else:
+            raise Exception("'business' is not a string or a dict")
+
+        return self._tips_by_item[key]
 
     def users(self):
         for user in self._users.values():
--- src/trust_indicators.py
+++ src/trust_indicators.py
@@ -4,12 +4,14 @@ for which coefficients will eventually be learned.
 """
 from collections import defaultdict
 
+
 class YelpTrustIndicators:
     """Calculate global trust indicators for all users.
 
     Also implements methods for calculating local trust indicators
     """
     LAST_YEAR = 2019
+
     def __init__(self, yelp_data):
         self._yelp_data = yelp_data
         self._indicators = defaultdict(dict)
@@ -29,11 +31,20 @@ class YelpTrustIndicators:
             raise KeyError(f"'{key}'")
         return indicators
 
+    def _addi(self, user, indicator_title, indicator_value):
+        """Add an indicator with given title for given user"""
+        self._indicators[user['user_id']][indicator_title] = indicator_value
+
     def _compute_indicators(self):
         self._compute_elite_years()
         self._compute_elite_per_year()
         self._compute_profile_up()
         self._compute_profile_up_per_year()
+        self._compute_fans()
+        self._compute_fans_per_year()
+        self._compute_visibility()
+        self._compute_global_feedback()
+        self._compute_global_norm_feedback()
 
     def _elite_year_count(self, user):
         if not user['elite']:
@@ -51,11 +62,12 @@ class YelpTrustIndicators:
 
     def _compute_elite_years(self):
         """Based on Mauro eq 13"""
-        max_elite_years = max(self._elite_year_count(u) for u in self._yelp_data.users())
+        max_elite_years = max(self._elite_year_count(u)
+                              for u in self._yelp_data.users())
 
         for u in self._yelp_data.users():
             elite_years = self._elite_year_count(u)
-            self._indicators[u['user_id']]['elite_years'] = elite_years/max_elite_years
+            self._addi(u, 'elite_years', elite_years / max_elite_years)
 
     def _compute_elite_per_year(self):
         """A new indicator, elite years w.r.t number of years on site"""
@@ -66,19 +78,87 @@ class YelpTrustIndicators:
                 continue
 
             years_on_site = self._years_on_site(u)
-            self._indicators[u['user_id']]['elite_years_per_year'] = elite_years / years_on_site
+            self._addi(u, 'elite_years_per_year', elite_years / years_on_site)
 
     def _profile_up_count(self, user):
-        return user['compliment_hot'] + user['compliment_more'] + user['compliment_writer'] + user['compliment_profile']
+        return (user['compliment_hot'] + user['compliment_more'] +
+                user['compliment_writer'] + user['compliment_profile'])
 
     def _compute_profile_up(self):
-        """Based on Muaro eq 14, but some changes because of yelp data"""
-        max_ups = max(self._profile_up_count(u) for u in self._yelp_data.users())
+        """Based on Mauro eq 14, but some changes because of yelp data"""
+        max_ups = max(self._profile_up_count(u)
+                      for u in self._yelp_data.users())
         for u in self._yelp_data.users():
-            self._indicators[u['user_id']]['profile_up'] = self._profile_up_count(u) / max_ups
+            self._addi(u, 'profile_up', self._profile_up_count(u) / max_ups)
 
     def _compute_profile_up_per_year(self):
-        """Based on Muaro eq 14, but also dividing by years on site first"""
-        max_ups_per_year = max(self._profile_up_count(u)/self._years_on_site(u) for u in self._yelp_data.users())
+        """Based on Mauro eq 14, normalized for years on site."""
+        max_ups_per_year = max(self._profile_up_count(u) /
+                               self._years_on_site(u)
+                               for u in self._yelp_data.users())
         for u in self._yelp_data.users():
-            self._indicators[u['user_id']]['profile_up_per_year'] = (self._profile_up_count(u) / self._years_on_site(u)) / max_ups_per_year
+            indi = (self._profile_up_count(u) /
+                    (self._years_on_site(u) * max_ups_per_year))
+            self._addi(u, 'profile_up_per_year', indi)
+
+    def _compute_fans(self):
+        """Based on Mauro eq 15"""
+        max_fans = max(u['fans'] for u in self._yelp_data.users())
+        for u in self._yelp_data.users():
+            self._addi(u, 'fans', u['fans'] / max_fans)
+
+    def _compute_fans_per_year(self):
+        """Based on Mauro eq 15, normalized for years on site"""
+        max_fans_per_year = max(u['fans'] / self._years_on_site(u)
+                                for u in self._yelp_data.users())
+        for u in self._yelp_data.users():
+            indi = u['fans'] / (self._years_on_site(u) * max_fans_per_year)
+            self._addi(u, 'fans_per_year', indi)
+
+    def _compute_visibility(self):
+        """Based on Mauro eq 16."""
+        max_ups = max(self._profile_up_count(u)
+                      for u in self._yelp_data.users())
+        for u in self._yelp_data.users():
+            activity_level = self._count_contributions(u)
+            indi = self._profile_up_count(u) / (max_ups * activity_level)
+            self._addi(u, 'visibility', indi)
+
+    def _count_global_feedback(self, user):
+        useful_count = 0
+        funny_count = 0
+        cool_count = 0
+        like_count = 0
+        for review in user['reviews']:
+            useful_count += review['useful']
+            funny_count += review['funny']
+            cool_count += review['cool']
+        for tip in user['tips']:
+            like_count += tip['compliment_count']
+        return useful_count + funny_count + cool_count + like_count
+
+    def _count_contributions(self, user):
+        return len(user['reviews']) + len(user['tips'])
+
+    def _compute_global_feedback(self):
+        """Based on Mauro eq 17"""
+        max_feedback = max(self._count_global_feedback(u)
+                           for u in self._yelp_data.users())
+        for u in self._yelp_data.users():
+            indi = self._count_global_feedback(u) / max_feedback
+            self._addi(u, 'global_feedback', indi)
+
+    def _compute_global_norm_feedback(self):
+        """Based on Mauro eq 17, normalized for number of contributions"""
+        max_feedback = max((self._count_global_feedback(u) /
+                            self._count_contributions(u))
+                           for u in self._yelp_data.users())
+        for u in self._yelp_data.users():
+            indi = (self._count_global_feedback(u) /
+                    (self._count_contributions(u) * max_feedback))
+            self._addi(u, 'global_feedback_norm', indi)
+
+    def global_feedback(self, user, item):
+        """Based on Mauro eq 18: feedback relative to an item"""
+        reviews = self._yelp_data.get_reviews_for_item(item)
+        tips = self._yelp_data.get_tips_for_item(item)
